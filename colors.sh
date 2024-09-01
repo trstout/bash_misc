8ffb6db844ce9d275b4484c920dbd0c81154764f
[redacted] <100223316+trstout@users.noreply.github.com>
2024-09-01 17:03:41 -0400

    Create colors.sh
     bash text and bg color template

---
+++ colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Text colors template
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Background colors
+bg_red='\033[0;41m'
+bg_green='\033[0;42m'
+bg_yellow='\033[0;43m'
+bg_blue='\033[0;44m'
+bg_magenta='\033[0;45m'
+bg_cyan='\033[0;46m'
+
+# Clear the color
+clear='\033[0m'
